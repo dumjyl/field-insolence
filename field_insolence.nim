9a0e6210c4810a5572721ce9708fdbfd3c3a5a6f
Jasper Jenkins <jasper.vs.jenkins@gmail.com>
2020-06-19 15:17:15 -0700

    cleanup

--- field_insolence.nim
+++ field_insolence.nim
@@ -1,42 +1,25 @@
 import macros
 
-proc find_field_rec(ast: NimNode, field: string): NimNode =
+proc find_field(ast: NimNode, field: string): NimNode =
    if ast.kind == nnkSym and ast.sym_kind == nskField and ast.eq_ident(field):
       return ast
    for i in 0 ..< ast.len:
-      result = find_field_rec(ast[i], field)
+      result = find_field(ast[i], field)
       if result != nil:
          return
 
-proc find_field(ast: NimNode, field: string): NimNode =
+macro impl(self: typed, field: static[string], ast: typed): auto =
    assert(ast.kind == nnkStmtList)
-   result = find_field_rec(ast, field)
+   let field_sym = find_field(ast, field)
    assert(result != nil, "failed to find field with the name: " & field)
-
-macro get_field(self: typed, field: static[string], ast: typed): auto =
-   let field = find_field(ast, field)
-   quote: `self`.`field`
-
-macro set_field(self: typed, field: static[string], val: typed, ast: typed): auto =
-   let field = find_field(ast, field)
-   quote: `self`.`field` = `val`
+   quote: `self`.`field_sym`
 
 template field*[T: object](self: T, field: static[string]): auto =
-   get_field self, field:
-      for field_val in self.fields:
-         discard field_val
-
-template `field=`*[T: object](self: T, field: static[string], val: auto): auto =
-   set_field self, field, val:
+   impl self, field:
       for field_val in self.fields:
          discard field_val
 
 template field*[T: ref object](self: T, field: static[string]): auto =
-   get_field self, field:
-      for field_val in self[].fields:
-         discard field_val
-
-template `field=`*[T: ref object](self: T, field: static[string], val: auto): auto =
-   set_field self, field, val:
+   impl self, field:
       for field_val in self[].fields:
          discard field_val
